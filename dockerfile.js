ba0de87d4306a47835d680b52a2ca197111fd74c
Guramritpal Singh <singh.guramritpal@yahoo.com>
2015-02-15 16:01:55 -0800

    Update dockerfile.js

--- dockerfile.js
+++ dockerfile.js
@@ -4,16 +4,16 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect("mongodb://Guramritpal:win123)(*&@ds043991.mongolab.com:43991/dockerfile", function(err, db) {
   if(err) { return console.dir(err); }
 
-  var collection = db.collection('test');
-  var doc = {mykey:1, fieldtoupdate:1};
+  var collection = db.collection('Amrit');
+  var doc = {name:Amrit, course:SE};
 
   collection.insert(doc, {w:1}, function(err, result) {
-    collection.update({mykey:1}, {$set:{fieldtoupdate:2}}, {w:1}, function(err, result) {});
+    collection.update({name:Amrit}, {$set:{fieldtoupdate:2}}, {name:Amrit}, function(err, result) {});
   });
 
-  var doc2 = {mykey:2, docs:[{doc1:1}]};
+  var doc2 = {name:Amritpal, docs:[{doc1:1}]};
 
-  collection.insert(doc2, {w:1}, function(err, result) {
-    collection.update({mykey:2}, {$push:{docs:{doc2:1}}}, {w:1}, function(err, result) {});
+  collection.insert(doc2, {name:Amrit}, function(err, result) {
+    collection.update({name:Amritpal}, {$push:{docs:{doc2:1}}}, {name:Amrit}, function(err, result) {});
   });
 });
